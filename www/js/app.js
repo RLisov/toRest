f6596f1826fb9ee0b6f429b1c246df9025980455
Andrew <block159@gmail.com>
2015-08-14 10:49:47 +0300

    more views

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('toRest', ['ionic', 'toRest.controllers', 'ionic.rating'])
+angular.module('toRest', ['ionic', 'toRest.controllers', 'ionic.rating', 'tabSlideBox'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -18,13 +18,41 @@ angular.module('toRest', ['ionic', 'toRest.controllers', 'ionic.rating'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+  $ionicConfigProvider.backButton.previousTitleText(false).text('');
+
   $stateProvider
 
   .state('home', {
     url: '/',
     templateUrl: 'templates/home.html',
     controller: 'MainCtrl'
+  })
+
+  .state('tourpage', {
+    url: '/tourpage',
+    /*abstract: 'true',*/
+    templateUrl: 'templates/tourpage.html',
+    controller: 'TourpageCtrl'
+  })
+
+/*  .state('tourpage.main', {
+    url: '/main',
+    templateUrl: 'templates/tourpage/_main.html',
+    controller: 'TourpageCtrl'
+  })
+
+  .state('tourpage.gallery', {
+    url: '/gallery',
+    templateUrl: 'templates/tourpage/_gallery.html',
+    controller: 'TourpageCtrl'
+  })*/
+
+  .state('favourites', {
+    url: '/favourites',
+    templateUrl: 'templates/favourites.html',
+    controller: 'FavouritesCtrl'
   })
 
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,21 @@
 angular.module('toRest.controllers', [])
 
-.controller('MainCtrl', function($scope) {
-  $scope.rate = 3;
-  $scope.max = 5;
+	.controller('FavouritesCtrl', function($scope) {
+	  $scope.rate = 3;
+	  $scope.max = 5;
+	})
 
-});
+	.controller('TourpageCtrl', function($scope, $ionicSlideBoxDelegate) {
+	  $scope.rate = 3;
+	  $scope.max = 5;
+	  $scope.choice = "A";
+
+	  $scope.gallerySlider = function($index) {
+	  	$ionicSlideBoxDelegate.$getByHandle('gallery').slide($index);
+	  }
+	})
+
+	.controller('MainCtrl', function($scope) {
+	  $scope.rate = 3;
+	  $scope.max = 5;
+	});
