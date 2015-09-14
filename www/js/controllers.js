9ca587330364e818660317c93e8628635062e94b
Ruslan <karimvrus2@gmail.com>
2015-09-14 12:57:35 +0300

    rmk googlemap

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -94,6 +94,31 @@ angular.module('toRest.controllers', [])
 	  $scope.choice = "A";
 	  $scope.tabIndex = 0;
 	  
+    //google initialize
+
+    $scope.map;
+        $scope.markers = [];
+        $scope.markerId = 1;
+
+        //Map initialization  
+        $timeout(function(){
+
+            var latlng = new google.maps.LatLng(35.7042995, 139.7597564);
+            var myOptions = {
+                zoom: 8,
+                center: latlng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            $scope.map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
+            $scope.overlay = new google.maps.OverlayView();
+            $scope.overlay.draw = function() {}; // empty function required
+            $scope.overlay.setMap($scope.map);
+            $scope.element = document.getElementById('map_canvas');
+            
+
+        },100);
+
+
     $timeout(function() {
       $ionicSlideBoxDelegate.$getByHandle('main-tabs').enableSlide(false);
     }, 10);
@@ -109,63 +134,7 @@ angular.module('toRest.controllers', [])
 	  	console.log('index gallerry', index);
 	  }
     
-    // initialize GOOGLE MAP
- //    function initialize() {
- //        var myLatlng = new google.maps.LatLng(43.07493,-89.381388);;
-        
- //        var mapOptions = {
- //          center: myLatlng,
- //          zoom: 16,
- //          mapTypeId: google.maps.MapTypeId.ROADMAP
- //        };
- //        var map = new google.maps.Map(document.getElementById("map"),
- //            mapOptions);
-        
- //        //Marker + infowindow + angularjs compiled ng-click
- //        var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
- //        var compiled = $compile(contentString)($scope);
-
- //        var infowindow = new google.maps.InfoWindow({
- //          content: compiled[0]
- //        });
-
- //        var marker = new google.maps.Marker({
- //          position: myLatlng,
- //          map: map,
- //          title: 'Uluru (Ayers Rock)'
- //        });
-
- //        google.maps.event.addListener(marker, 'click', function() {
- //          infowindow.open(map,marker);
- //        });
-
- //        $scope.map = map;
- //      }
- //      google.maps.event.addDomListener(window, 'load', initialize);
-      
- //      $scope.centerOnMe = function() {
- //        if(!$scope.map) {
- //          console.log('me here');
- //          return;
- //        }
-
- //        $scope.loading = $ionicLoading.show({
- //          content: 'Getting current location...',
- //          showBackdrop: false
- //        });
-
- //        navigator.geolocation.getCurrentPosition(function(pos) {
- //          $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
- //          $scope.loading.hide();
- //        }, function(error) {
- //          alert('Unable to get location: ' + error.message);
- //        });
- //      };
-      
- //      $scope.clickTest = function() {
- //        alert('Example of infowindow with ng-click')
- //      };
-
+   
 	})
 
 	
